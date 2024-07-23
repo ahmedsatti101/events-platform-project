aa762575a38c407d08920ea4624e849b802df106
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-07-23 11:56:08 +0100

    All required info for event is displayed

--- src/components/SingleEvent.tsx
+++ src/components/SingleEvent.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useParams } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export default function SingleEvent() {
   const { event_id } = useParams();
@@ -27,10 +28,41 @@ export default function SingleEvent() {
     }
   }, [event_id]);
 
+  const copyUrl = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+    alert("Link copied")
+  }
+
   return (
     <>
       {event.map((e) => {
-        return <p>{e.title}</p>;
+        return (
+          <>
+            <h2>Description</h2>
+            <article id="event-description">{e.description}</article>
+
+            <h3>Date</h3>
+            <p>{e.date}</p>
+
+            <h3>Time</h3>
+            <p>{`${e.startTime} - ${e.endTime}`}</p>
+
+            <h3>Where</h3>
+            <p>{e.location}</p>
+
+            <Button variant="contained" id="sign-up-button">
+              Sign up
+            </Button>
+
+            <button onClick={copyUrl}>Copy link</button>
+
+            <p>
+              For queries or issues you can send a message to {e.email} or
+              phone {e.phoneNumber}
+            </p>
+          </>
+        );
       })}
     </>
   );
