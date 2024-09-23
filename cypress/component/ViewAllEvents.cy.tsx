9f3c6288293d03ce8b1fb11bfb21458d2e5232b8
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-09-23 17:22:18 +0100

    Done writing cypress tests for Events.tsx

--- cypress/component/ViewAllEvents.cy.tsx
+++ cypress/component/ViewAllEvents.cy.tsx
@@ -7,11 +7,48 @@ beforeEach(() => {
 });
 
 describe('ViewAllEvents.cy.tsx', () => {
-  it('Users are able to see listed events', () => {
-      cy.get('[data-testid=events]').should('be.visible')
-  })
+    describe("Events card attributes", () => {
+        it('Event cards should be visible on screen', () => {
+            cy.get('[data-testid=events]').should('be.visible')
+        })
 
-  it('Should display correct event title', () => {
-      cy.get('[id=event-header]').contains("Test")
-  })
+        it('Should display correct event title', () => {
+            const eventHeader = "event-header";
+            cy.get(`[id=${eventHeader}]`).should("be.visible")
+            cy.get(`[id=${eventHeader}]`).contains("Test event")
+        })
+
+        it("Event location should be visible on screen", () => {
+            const eventLocation = "event-location";
+            cy.get(`[id=${eventLocation}]`).should("be.visible")
+            cy.get(`[id=${eventLocation}]`).should("have.text", "Test location");
+        });
+
+        it("Expand icon should be clickable", () => {
+            const icon = "expand-more-icon";
+            cy.get(`[data-testid=${icon}]`).should("be.visible");
+            cy.get(`[data-testid=${icon}]`).click();
+        });
+
+        it("Event description should be visible", () => {
+            cy.get("[data-testid=expand-more-icon]").click();
+            const id = "event-description";
+            cy.get(`[id=${id}]`).should("be.visible");
+            cy.get(`[id=${id}]`).should("have.text", "Test description");
+        });
+
+        it("'Learn more' text should be visible on screen", () => {
+            const eventActions = "event-actions";
+            const children = cy.get(`[id=${eventActions}]`).children().get("p");
+
+            cy.get(`[id=${eventActions}]`).should("be.visible")
+            children.should("contain.text", "Learn more")
+        });
+
+        it("Sign up button should be clickable", () => {
+            const id = "sign-up-button";
+            cy.get(`[id=${id}]`).should("be.visible");
+            cy.get(`[id=${id}]`).click();
+        });
+    })
 })
